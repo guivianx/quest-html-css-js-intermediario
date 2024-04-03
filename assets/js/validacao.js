aa131961a019de71dd390c7a270eaf041f49b038
Guilherme Viana <guivianx@gmail.com>
2024-04-03 11:31:00 -0300

    pequenos ajustes na estilização

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -6,16 +6,16 @@ let i = 0
 
 inputs.forEach(input => {
     input.addEventListener('keyup', () => {
-        input.style.border = '2px solid #00c22b'
+        input.style.border = '1px solid #00c22b'
         if (input.value.length === 0) {
-            input.style.border = '2px solid'
+            input.style.border = '1px solid'
         }
     })
 
     botao.addEventListener('click', () => {
         
         if (input.value.length === 0) {
-            input.style.border = '2px solid red'
+            input.style.border = '1px solid red'
             paragrafo[i].innerHTML = 'campo obrigatório'
         }
         i++
