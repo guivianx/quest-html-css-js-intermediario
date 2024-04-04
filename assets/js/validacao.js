e285d27b541f315ccd7820caa6e37a9343df37a5
Guilherme Viana <guivianx@gmail.com>
2024-04-04 06:55:39 -0300

    codigo js refeito, corrigido erro de lógica

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -1,24 +1,32 @@
-const inputs = document.querySelectorAll('.input')
 const botao = document.getElementById('botaoEnviar')
-let paragrafo = document.querySelectorAll('.txt')
-
-let i = 0
+const inputs = document.querySelectorAll('.input')
 
 inputs.forEach(input => {
     input.addEventListener('keyup', () => {
-        input.style.border = '1px solid #00c22b'
-        if (input.value.length === 0) {
-            input.style.border = '1px solid'
-        }
-    })
-
-    botao.addEventListener('click', () => {
-        
-        if (input.value.length === 0) {
-            input.style.border = '1px solid red'
-            paragrafo[i].innerHTML = 'campo obrigatório'
-        }
-        i++
+        input.value.length === 0 ? input.style.border = "1px solid" : input.style.border = '1px solid #00c22b'
     })
 })
 
+botao.addEventListener('click', () => {
+
+    inputs.forEach(input => {
+        input.addEventListener('keyup', () => {
+
+            if (input.value.length === 0) {
+                input.nextElementSibling.classList.add("ativo")
+                input.style.border = '1px solid #f52e2e'
+            } else {
+                input.nextElementSibling.classList.remove("ativo")
+            }
+        })
+
+        let validacao = input.value
+
+        if (validacao == 0) {
+            input.nextElementSibling.classList.add("ativo")
+            input.style.border = '1px solid #f52e2e'
+        } else {
+            input.nextElementSibling.classList.remove("ativo")
+        }
+    })
+})
